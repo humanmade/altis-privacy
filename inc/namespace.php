789dca2f91b821d47e03fe4504089b7b6dd4cafc
Chris Reynolds <chris@jazzsequence.com>
2020-12-08 12:43:47 -0700

    add inline docs

--- inc/namespace.php
+++ inc/namespace.php
@@ -1,7 +1,15 @@
 <?php
+/**
+ * Main privacy functions.
+ *
+ * @package altis/privacy
+ */
 
 namespace Altis\Privacy;
 
+/**
+ * Bootstrap privacy functions as necessary.
+ */
 function bootstrap() {
 	Consent\bootstrap();
 }
