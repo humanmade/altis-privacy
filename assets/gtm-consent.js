3e1e267951b25a7dea328ef05812585e1bd1e443
Chris Reynolds <chris@jazzsequence.com>
2020-12-16 14:38:59 -0700

    listen for consent changes

--- assets/gtm-consent.js
+++ assets/gtm-consent.js
@@ -7,3 +7,10 @@ var Altis = window.Altis || {}; // eslint-disable-line no-var
 
 // Set existing consent statuses, if there are any.
 window[altisConsentGtm.dataLayer].push( 'consentInit', Altis.Consent.getCategories );
+
+// Listen for changes in consent status.
+document.addEventListener( 'wp_listen_for_consent_change', function ( e ) {
+	const dataLayer = altisConsentGtm.dataLayer;
+
+	window[dataLayer].push( 'consentChange', e.detail );
+} );
