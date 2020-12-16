a317827a88fed91197834524d9cf54b6f3adcfc3
Chris Reynolds <chris@jazzsequence.com>
2020-12-16 14:38:50 -0700

    set the initial altis consent

--- assets/gtm-consent.js
+++ assets/gtm-consent.js
@@ -4,3 +4,6 @@
 
 // Use the Altis namespace. Since it's declared as a var, we can't redeclare as a let or const.
 var Altis = window.Altis || {}; // eslint-disable-line no-var
+
+// Set existing consent statuses, if there are any.
+window[altisConsentGtm.dataLayer].push( 'consentInit', Altis.Consent.getCategories );
