bf5c9e467974ec6367b5ed907f6ab09593ea8176
Chris Reynolds <chris@jazzsequence.com>
2020-12-16 14:38:33 -0700

    use the Altis global namespace

---
+++ assets/gtm-consent.js
@@ -0,0 +1,6 @@
+/**
+ * Send consent status to GTM.
+ */
+
+// Use the Altis namespace. Since it's declared as a var, we can't redeclare as a let or const.
+var Altis = window.Altis || {}; // eslint-disable-line no-var
