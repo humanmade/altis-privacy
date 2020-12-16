d4b25f1024b3fff22b1e201419af1ea22e92d686
Chris Reynolds <chris@jazzsequence.com>
2020-12-16 13:07:52 -0700

    add base gtm namespace

---
+++ inc/gtm/namespace.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Altis Consent Google Tag Manager Integration
+ *
+ * @package altis/privacy
+ */
+
+namespace Altis\Privacy\GTM;
+
+use Altis;
+
+function bootstrap() {
+	add_action( 'plugins_loaded', __NAMESPACE__ . '\\init' );
+}
+
+function init() {
+	$consent = Altis\get_config()['modules']['privacy']['consent'];
+	$gtm     = Altis\get_config()['modules']['analytics']['google-tag-manager'];
+
+	// Bail if we aren't using the Consent API or GTM.
+	if ( ! $consent || ! $gtm ) {
+		return;
+	}
+
+	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
+}
+
+function enqueue_scripts() {
+	wp_enqueue_script( 'gtm-consent', plugins_url( '/assets/gtm-consent.js', dirname( __FILE__ ) ), [], '6.0', true );
+}
