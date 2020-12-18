f5ee694a8660036b22c3992578c1e703483da7e3
Robert O'Rourke <robert@o-rourke.org>
2020-12-18 15:59:42 +0000

    Add Altis Analytics integration
    This filters the consent cookie prefix value to make Altis Analytics work with the WP Consent API.
    
    Related to #3
    
    Dependent on https://github.com/humanmade/aws-analytics/pull/131

--- inc/consent/namespace.php
+++ inc/consent/namespace.php
@@ -8,23 +8,43 @@
 namespace Altis\Privacy\Consent;
 
 use Altis;
+use Altis\Consent;
 
 /**
  * Kick it off.
  */
 function bootstrap() {
+	$config = Altis\get_config()['modules']['privacy']['consent'];
+
+	// Unless the consent module has been deactivated, load the plugins.
+	if ( empty( $config ) ) {
+		return;
+	}
+
 	add_action( 'plugins_loaded', __NAMESPACE__ . '\\load_plugins', 1 );
+
+	// Filter native analytics consent cookie prefix.
+	add_filter( 'altis.analytics.consent_cookie_prefix', __NAMESPACE__ . '\\enable_analytics_consent' );
 }
 
 /**
  * Load plugins that are part of the consent module.
  */
 function load_plugins() {
-	$config = Altis\get_config()['modules']['privacy']['consent'];
+	require_once Altis\ROOT_DIR . '/vendor/altis/consent-api/plugin.php';
+	require_once Altis\ROOT_DIR . '/vendor/altis/consent/plugin.php';
+}
 
-	// Unless the consent module has been deactivated, load the plugins.
-	if ( $config ) {
-		require_once Altis\ROOT_DIR . '/vendor/altis/consent-api/plugin.php';
-		require_once Altis\ROOT_DIR . '/vendor/altis/consent/plugin.php';
+/**
+ * Filter the Analytics plugin consent cookie prefix.
+ *
+ * @param string|null $cookie_prefix The analytics consent cookie prefix or null.
+ * @return string|null
+ */
+function enable_analytics_consent( ?string $cookie_prefix ) {
+	if ( ! Consent\should_display_banner() ) {
+		return $cookie_prefix;
 	}
+
+	return Consent\cookie_prefix();
 }
