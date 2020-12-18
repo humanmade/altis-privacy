8cf221d6076b91a4aa8569b02725a1fd5479eb80
Chris Reynolds <chris@jazzsequence.com>
2020-12-18 15:01:13 -0700

    add doc comments

--- inc/gtm/namespace.php
+++ inc/gtm/namespace.php
@@ -9,6 +9,9 @@ namespace Altis\Privacy\GTM;
 
 use Altis;
 
+/**
+ * Kick it off.
+ */
 function bootstrap() {
 	$consent = Altis\get_config()['modules']['privacy']['consent'];
 	$gtm     = Altis\get_config()['modules']['analytics']['google-tag-manager'];
@@ -21,6 +24,9 @@ function bootstrap() {
 	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_scripts' );
 }
 
+/**
+ * Enqueue the GTM consent script and pass through any variables we need on the client side.
+ */
 function enqueue_scripts() {
 	wp_enqueue_script( 'gtm-consent', plugins_url( '/assets/gtm-consent.js', dirname( __FILE__, 2 ) ), [], '6.0', true );
 
